5f793e1ea3a81eec29d2a94bab900ee42d76b0a9
amirhayati <amir_hossein_hayati@yahoo.com>
2023-04-01 23:24:43 +0330

    add commit

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,121 @@
+import {useState} from 'react'
+import pallet from '../../theme'
+import { Box, AppBar, Toolbar, Typography, CssBaseline, Divider, Drawer, List, ListItem, ListItemButton, ListItemText, Button } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import Btn from '../btn/Btn'
+import './nav.css'
+
+
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Portfolio', 'Contact', 'Blog'];
+
+const Nav =(props)=>{  
+
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
+
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+                Portfolio
+            </Typography>
+            <Divider />
+            <List>
+                <Box
+                    className={"navItemMobile"}
+                    sx={{
+                        '& li':{
+                        width:'80%'
+                        },
+                        '& a':{
+                            color: pallet.white[900],
+                            width:'100%'
+                        },
+                        '&>:hover': {
+                            color: pallet.yellow[500],
+                            cursor: 'pointer',
+                            boxShadow: `0 2px 0 ${pallet.yellow[500]}`,
+                        }
+                    }}
+                >
+                    <li className='header_link'><a to={"/"} >Home</a></li>
+                    <li className='header_link'><a to={"/About"} >About</a></li>
+                    <li className='header_link'><a to={"/Portfolio"} >Portfolio</a></li>
+                    <li className='header_link'><a to={"/Contact"} >Contact</a></li>
+                    <li className='header_link'><a to={"/Blog"} >Blog</a></li>
+                </Box>
+            </List>
+            <Btn>Download CV</Btn>
+        </Box>
+    );
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    return (
+        <Box>
+
+            {/* ------ header content ------ */}
+            <Box sx={{ display: { xs: 'none', sm: 'none', md: 'flex' }}} id={"nav"}> 
+                <h2>Portfolio</h2>
+                <Box
+                    className={"navItem"}
+                    sx={{
+                        '& a':{
+                            color: pallet.white[900],
+                        },
+                        '&>:hover': {
+                            color: pallet.yellow[500],
+                            cursor: 'pointer',
+                            boxShadow: `0 2px 0 ${pallet.yellow[500]}`,
+                        }
+                    }}
+                >
+                    <li className='header_link'><a to={"/"} >Home</a></li>
+                    <li className='header_link'><a to={"/About"} >About</a></li>
+                    <li className='header_link'><a to={"/Portfolio"} >Portfolio</a></li>
+                    <li className='header_link'><a to={"/Contact"} >Contact</a></li>
+                    <li className='header_link'><a to={"/Blog"} >Blog</a></li>
+                </Box>
+                <Btn>Download CV</Btn>
+            </Box>
+
+             {/* ------ drawer btn ------ */}
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="end"
+                onClick={handleDrawerToggle}
+                sx={{ position: 'absolute',right: 0,margin: '1rem', display: { md: 'none' } }}
+            >
+                <MenuIcon />
+            </IconButton> 
+            
+            {/* ------ on drawer btn open ------ */}
+            <Box component="nav">
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { sm: 'block', md: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </Box>
+    </Box>
+        
+  );
+}
+
+export default Nav;
